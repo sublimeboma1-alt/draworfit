f7be5650c3ad59d1795dd6145da544881f9e5173
joseph <sublimeboma1@gmail.com>
2026-09-01 13:17:22 +0200

    MAJEUR

--- backend/les_apps/documents/models.py
+++ backend/les_apps/documents/models.py
@@ -36,7 +36,13 @@ class Document(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            base_slug = slugify(self.title) or 'document'
+            slug = base_slug
+            number = 2
+            while type(self).objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f'{base_slug}-{number}'
+                number += 1
+            self.slug = slug
         super().save(*args, **kwargs)
 
     def __str__(self):
